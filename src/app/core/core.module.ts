d9f9c84eb65baa4c545054e156ea1b0ddbd0eb7c
Michal Kobuszewski <m.kobuszewski99@gmail.com>
2022-12-02 18:44:52 +0100

    wip

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
 export class CoreModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../core/components/header/header.component';
-import { FooterComponent } from '../core/components/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
-import { YourProductsPageComponent } from './components/your-products-page/your-products-page.component';
 import { DailyDietPageComponent } from './components/daily-diet-page/daily-diet-page.component';
 import { ProductsPageComponent } from './components/products-page/products-page.component';
 
@@ -11,10 +8,7 @@ import { ProductsPageComponent } from './components/products-page/products-page.
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    FooterComponent,
     AboutComponent,
-    YourProductsPageComponent,
     DailyDietPageComponent,
     ProductsPageComponent
   ],
